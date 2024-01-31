875d08941d99efd6d57751835c04178d6f8d7c44
Rohan Kewalramani <45906368+rohankewal@users.noreply.github.com>
2024-01-31 09:58:43 -0500

    module: added sqlite3 for database use later on

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
+
+# Main/Home page route
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
