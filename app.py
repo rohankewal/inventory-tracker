bee6f6edab5165ee6eadcf55cf6949ef3e8c5833
Rohan Kewalramani <45906368+rohankewal@users.noreply.github.com>
2024-02-02 14:00:34 -0500

    test: tested dynamic URL with flask and it worked! 🎉

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
@@ -7,7 +7,8 @@ app = Flask(__name__)
 # Main/Home page route
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    name = request.args["name"]
+    return render_template("index.html", first_name=name)
 
 
 if __name__ == "__main__":
