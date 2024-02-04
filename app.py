88e43e10dc2a7e9e4a1ee5ef2e089cbe06e7837b
Rohan Kewalramani <45906368+rohankewal@users.noreply.github.com>
2024-02-04 08:30:22 -0500

    refct: changed function name to something more understandable

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 # Main/Home page route
 @app.route("/")
-def hello_world():
-    name = request.args["name"]
+def index():
+    name = request.args.get("name", "Guest")
     return render_template("index.html", first_name=name)
 
 
