00fcf39775ef540393e3f25b2199b021b87aabd7
Rohan Kewalramani <45906368+rohankewal@users.noreply.github.com>
2024-02-06 13:35:52 -0500

    feat: created database for users

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template, request
 import sqlite3
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
+
+
+# TODO: Setup database for creating and logging in users
+conn = sqlite3.connect("users.db")
+cursor = conn.cursor()
+cursor.execute(
+    """CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    firstname TEXT NOT NULL,
+    lastname TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL
+    )"""
+)
 
 
 # Main/Home page route
